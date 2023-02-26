c4f49d8b47c3dc5d2b786822c28e38abc690c0cd
Lowkeyoj <rockyojaji@gmail.com>
2023-02-25 17:50:11 -0800

    First commit

---
+++ src/page1.js
@@ -0,0 +1,15 @@
+header = (() => {
+    const header = document.createElement("header");
+    const h1 = document.createElement("h1");
+    const nav = document.createElement("nav");
+    const ul = document.createElement("ul");
+    const li1 = document.createElement("li");
+    const li2 = document.createElement("li");
+    const li3 = document.createElement("li");
+
+    h1.textContent = "welcome to my restruant";
+
+    
+
+}
+)();
